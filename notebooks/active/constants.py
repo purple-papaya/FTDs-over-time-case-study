33feba4562cc61c8c1de462489ee93332a8eb959
Pavlina Novakova <paulinenovak@icloud.com>
2026-01-21 10:44:28 +0100

    feat: handle referential integrity

--- notebooks/active/constants.py
+++ notebooks/active/constants.py
@@ -6,7 +6,7 @@ DATAFRAMES = {
     'df_trans': load_csv(PATH + 'trans.csv', parse_dates=['created_at']),
     'df_trader': load_csv(PATH + 'trader.csv'),
     'df_clients': load_csv(PATH + 'clients.csv', parse_dates=['_created_on']),
-    'df_partner_codes': load_csv(PATH + 'partner_codes.csv'),
+    'df_partner_codes': load_csv(PATH + 'partner_codes.csv', parse_dates=['_created_on']),
     'df_a2p_ref': load_csv(PATH + 'a2p_ref.csv')
 } 
 
